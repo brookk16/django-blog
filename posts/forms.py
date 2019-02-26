caaaed1a8620647bbf2e56f33c8294167ecdf214
Brook Kynaston <brookkynaston@live.fr>
2019-02-26 17:15:11 +0000

    Testing deployment

--- posts/forms.py
+++ posts/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from .models import Post
 
 class BlogPostForm(forms.ModelForm):
-    
     class Meta:
         model = Post
-        fields = ('title', 'content', 'tags', 'image', 'published_date') 
-        
-    
+        fields = ('title', 'content', 'image', 'tag', 'published_date')
