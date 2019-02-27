d8f2b574b396b126883b00d998213d35679899b9
Brook Kynaston <brookkynaston@live.fr>
2019-02-27 19:38:05 +0000

    Setting up heroku deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "'14+087yylrqc8q9=(!$p5y7$f&3=mb#yr@@g*%#-%wdw$boz%7'")
+
+os.environ.setdefault("DATABASE_URL", "postgres://gvhgrmhopfklfg:036efd22706db123e70057e7b6ebc25b9427a64fc25f148d64c207d8b5261c1f@ec2-54-217-207-242.eu-west-1.compute.amazonaws.com:5432/d6l0rrq37s8b04")
